73b11bfa9e6ac3b44e3095c10dc406179583ed0d
Lina Escobar <lina.escobar@wizard.com.co>
2021-08-25 17:24:10 -0500

    TemplateScene

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    public void ScaleObject(float scaleValue)
+	{
+		Vector3 newScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * scaleValue;
+		transform.localScale = newScale;
+	}
+
+	public void ResetScale()
+	{
+		transform.localScale = Vector3.one;
+	}
+
+	public void ChangeMaterial()
+	{
+		
+	}
+
+
+}
