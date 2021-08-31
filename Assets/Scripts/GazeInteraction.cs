f873a3594aae47a3b85e6ec5335a20af5b206648
Lina Escobar <lina.escobar@wizard.com.co>
2021-08-31 16:32:04 -0500

    GazeInteraction Fix

--- Assets/Scripts/GazeInteraction.cs
+++ Assets/Scripts/GazeInteraction.cs
@@ -11,6 +11,16 @@ public class GazeInteraction : MonoBehaviour
     private GameObject _gazeObject;
     private PointerEventData _eventData;
 
+	private void OnEnable()
+	{
+        ReticleManager.ReticleFilled += OnClick;
+	}
+
+    private void OnDisable()
+    {
+        ReticleManager.ReticleFilled -= OnClick;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,23 +33,28 @@ public class GazeInteraction : MonoBehaviour
         UpdateInteraction();
     }
 
-    
+
     void UpdateInteraction()
 	{
         if(Physics.Raycast(transform.position, transform.forward, out var hit, interactableDistance))
 		{
-            if(_gazeObject != hit.transform.gameObject)
-			{
+           if(_gazeObject == null)
+		    {
                 _gazeObject = hit.transform.gameObject;
-
-                if(_gazeObject)
+                _gazeObject.GetComponent<IPointerEnterHandler>()?.OnPointerEnter(_eventData);
+            }
+			else if (_gazeObject != hit.transform.gameObject)
+			{
+                if (_gazeObject)
 				{
                     _gazeObject.GetComponent<IPointerExitHandler>()?.OnPointerExit(_eventData);
                     _gazeObject = hit.transform.gameObject;
                     _gazeObject.GetComponent<IPointerEnterHandler>()?.OnPointerEnter(_eventData);
                 }
 
-			}
+
+            }
+
 		}
         else if(_gazeObject)
 		{
@@ -50,7 +65,12 @@ public class GazeInteraction : MonoBehaviour
 
         if(_gazeObject !=null && Google.XR.Cardboard.Api.IsTriggerPressed)
 		{
-            _gazeObject.GetComponent<IPointerClickHandler>()?.OnPointerClick(_eventData);
+            ClickEvent();
 		}
 	}
+
+    public void OnClick()
+	{
+        _gazeObject.GetComponent<IPointerClickHandler>()?.OnPointerClick(_eventData);
+    }
 }
